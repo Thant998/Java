da6069c23def261357d21944e5508450f42d8749
uHigh <95152361+Thant998@users.noreply.github.com>
2022-06-22 11:32:27 +0630

    Add files via upload

--- ServletFilter/UsefulFilter/Filter_1.java
+++ ServletFilter/UsefulFilter/Filter_1.java
@@ -1,1 +1,19 @@
+//Sending response by filter only
 
+import java.io.*;  
+import javax.servlet.*;  
+  
+public class Filter_1 implements Filter{  
+    public void init(FilterConfig arg0) throws ServletException {}  
+  
+    public void doFilter(ServletRequest req, ServletResponse res,  
+            FilterChain chain) throws IOException, ServletException {  
+      
+        PrintWriter out=res.getWriter();  
+          
+        out.print("<br/>this site is underconstruction..");  
+        out.close();  
+          
+    }  
+    public void destroy() {}  
+}  
