3b634c0800784c1b990d2835d73879bccd061951
Hide Lost <95152361+Thant998@users.noreply.github.com>
2022-07-17 16:16:43 +0630

    Add files via upload

---
+++ BookManagement-jsp-servlet-html-project/src/bookmanagement.model/BookBean.java
@@ -0,0 +1,33 @@
+package bookmanagement.model;
+
+public class BookBean {
+	private String code;
+	private String title;
+	private String author;
+	private String prize;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPrize() {
+		return prize;
+	}
+	public void setPrize(String prize) {
+		this.prize = prize;
+	}
+	
+}
