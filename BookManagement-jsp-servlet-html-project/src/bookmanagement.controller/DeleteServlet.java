61eada16d8774df48117e77b41f5f9dee5c8ddd3
Hide Lost <95152361+Thant998@users.noreply.github.com>
2022-07-17 16:22:47 +0630

    Add files via upload

---
+++ BookManagement-jsp-servlet-html-project/src/bookmanagement.controller/DeleteServlet.java
@@ -0,0 +1,53 @@
+package bookmanagement.controller;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookmanagement.model.BookBean;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String code=request.getParameter("code");
+		List<BookBean> list=(List<BookBean>) request.getServletContext().getAttribute("list");
+		Iterator<BookBean> itr=list.iterator();
+		while (itr.hasNext()) {
+			if (itr.next().getCode().equals(code)) {
+				itr.remove();
+			}
+		}
+		request.getRequestDispatcher("displayBook.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
