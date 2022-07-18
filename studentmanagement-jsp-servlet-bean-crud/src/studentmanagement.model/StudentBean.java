f4e4bb2e6a3340d7dd5745872e0da077bb2e43e4
Hide Lost <95152361+Thant998@users.noreply.github.com>
2022-07-18 14:56:48 +0630

    Add files via upload

---
+++ studentmanagement-jsp-servlet-bean-crud/src/studentmanagement.model/StudentBean.java
@@ -0,0 +1,33 @@
+package studentmanagement.model;
+
+public class StudentBean {
+	private String id;
+	private String name;
+	private String dob;
+	private String subjects;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getSubjects() {
+		return subjects;
+	}
+	public void setSubjects(String subjects) {
+		this.subjects = subjects;
+	}
+	
+}
