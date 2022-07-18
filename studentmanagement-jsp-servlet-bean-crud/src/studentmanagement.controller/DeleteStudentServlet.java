fe9c131e4c39096a6a41d531c00c50d7c3d469cd
Hide Lost <95152361+Thant998@users.noreply.github.com>
2022-07-18 14:58:05 +0630

    Add files via upload

---
+++ studentmanagement-jsp-servlet-bean-crud/src/studentmanagement.controller/DeleteStudentServlet.java
@@ -0,0 +1,54 @@
+package studentmanagement.controller;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import studentmanagement.model.StudentBean;
+
+/**
+ * Servlet implementation class DeleteStudentServlet
+ */
+@WebServlet("/DeleteStudentServlet")
+public class DeleteStudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteStudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id=request.getParameter("id");
+		List<StudentBean> list=(List<StudentBean>) request.getServletContext().getAttribute("list");
+		Iterator<StudentBean> itr=list.iterator();
+		while (itr.hasNext()) {
+			if (itr.next().getId().equals(id)) {
+				itr.remove();
+			}
+		}
+		request.getRequestDispatcher("displaystudent.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
