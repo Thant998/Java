feaa52fdecc5a0ff5f686438b75ea3d31e760e1a
Cry_Babe_ <95152361+Thant998@users.noreply.github.com>
2022-07-25 18:50:50 +0630

    Add files via upload

---
+++ UserManagement_CRUD/usermanagement.dto/UserRequestDTO.java
@@ -0,0 +1,32 @@
+package usermanagement.dto;
+
+public class UserRequestDTO {
+	private String userId;
+	private String userName;
+	private String userEmail;
+	private String userCountry;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserCountry() {
+		return userCountry;
+	}
+	public void setUserCountry(String userCountry) {
+		this.userCountry = userCountry;
+	}
+}
